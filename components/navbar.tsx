27da5da2de93e08dced7e54caa4a49f2cf2182b8
Reza Firdaus <azerus98@gmail.com>
2022-11-01 17:51:55 +0700

    create structure

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,6 @@
-const Navbar = () => {
+import Link from "next/link"
+
+const Navbar = (): JSX.Element => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
@@ -9,10 +11,10 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About Me</a>
+              <Link className="nav-link" href="/about-me">About Me</Link>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -30,7 +32,7 @@ const Navbar = () => {
             </li>
           </ul>
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <input className="form-control me-2 bg-dark" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Search</button>
           </form>
         </div>
