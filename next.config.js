a3cc190bae3401f88d3fe0ceaa99108f70309507
Reza Firdaus <azerus98@gmail.com>
2022-11-03 11:08:58 +0700

    fix add base path for github

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  basePath: 'porto-reza'
+  swcMinify: false,
+  basePath: '/porto-reza'
 }
 
 module.exports = nextConfig
