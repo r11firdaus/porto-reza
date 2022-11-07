5d83c073dc40e2a0418acb61d6fe23df2a79ae11
Reza Firdaus <azerus98@gmail.com>
2022-11-07 09:59:50 +0700

    add education and pdf link

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,20 +1,19 @@
 import Navbar from "../components/navbar"
 import cvData from "../dummy/cv.json"
 
-const cv = (): JSX.Element => {
+const Cv = (): JSX.Element => {
   return (
     <div className="bg-dark text-light px-2">
       <Navbar />
-      <h2 className="card-title mt-3 mx-3">Curriculum Vitae</h2>
       {/* Experience */}
       <section className="resume-section" id="experience">
         <div className="resume-section-content">
-          <h3 className="mb-5"><i>Experience</i></h3>
+          <h3 className="mb-5"><i><b>Experience</b></i></h3>
           {cvData.experience.map((cv) => (
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
               <div className="flex-grow-1">
                   <h5 className="mb-0">{cv.position}</h5>
-                  <div className="subheading mb-3">{cv.company}</div>
+                  <div className="subheading mb-2 text-secondary">{cv.company}</div>
                   <p>{cv.detail}</p>
               </div>
               <div className="flex-shrink-0"><span className="text-success">{`${cv.start_date} - ${cv.end_date}`}</span></div>
@@ -22,8 +21,30 @@ const cv = (): JSX.Element => {
           ))}
         </div>
       </section>
+
+      {/* Education */}
+      <section className="resume-section" id="education">
+        <div className="resume-section-content">
+          <h3 className="mb-5"><i><b>Education</b></i></h3>
+          {cvData.education.map((cv) => (
+            <div className="d-flex flex-column flex-md-row justify-content-between mb-3">
+              <div className="flex-grow-1">
+                  <h5 className="mb-0">{cv.level}</h5>
+                  <div className="subheading mb-2 text-secondary">{cv.company}</div>
+                  <p>{cv.detail}</p>
+              </div>
+              <div className="flex-shrink-0"><span className="text-success">{`${cv.start_date} - ${cv.end_date}`}</span></div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* PDF LINK */}
+      <section className="resume-section" id="pdf_link">
+        <p>For full detail about my CV, <a href={cvData.pdf_link} target="_blank"><u>Cilck Here</u></a></p>
+      </section>
     </div>
   )
 }
 
-export default cv
+export default Cv
